5e70f53560f425b253d122e73b7ba558cc124a49
Aleksandar Milanovic <milanovicaleX77@gmail.com>
2022-05-05 21:44:00 +0200

    Test active scanner

---
+++ src/test/java/com/joyodev/aye/activescanner/ActiveScannerClientTest.java
@@ -0,0 +1,24 @@
+package com.joyodev.aye.activescanner;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ActiveScannerClientTest {
+
+    @Test
+    void testClientSuccessfullyCreated() {
+        assertDoesNotThrow(() -> new ActiveScannerClient("https://localhost:8228/v1"));
+    }
+
+    @Test
+    void testClientNotCreated() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            new ActiveScannerClient("some.bad.url/v1");
+        });
+        String expectedMessage = "Provided Anchore Engine URL is not valid: some.bad.url/v1";
+        String actualMessage = exception.getMessage();
+        assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+}
